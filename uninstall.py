32c8c58a7c2d4bccfd82cbc42da4d3103376197d
Naz <exoxe@msn.com>
2024-09-16 17:38:11 +0900

    update

--- uninstall.py
+++ uninstall.py
@@ -1,4 +1,5 @@
-#import os, stat
+import os
+import shutil
 
 annotating_file_list = ["../efficiency-nodes-comfyui/js/node_options/addLinks.js",
             "../efficiency-nodes-comfyui/js/node_options/addScripts.js",
@@ -10,9 +11,10 @@ annotating_file_list = ["../efficiency-nodes-comfyui/js/node_options/addLinks.js
             "../efficiency-nodes-comfyui/js/node_options/swapScripts.js",
             "../efficiency-nodes-comfyui/js/node_options/swapXYinputs.js"]
 
-read_user_css = "./user_css/user.css"
-write_user_css = "../../web/user.css"
-
+read_css_folder = "./user_css/"
+write_css_folder = "../../web/"
+user_css = "user.css"
+    
 def annotate_file(js_file):
     contents = []
     with open(js_file, 'r', encoding='UTF8') as f:
@@ -37,18 +39,20 @@ def restore_annotate_file(js_file):
             else:
                 f.write(c)
 
-def make_user_css():
-    contents = []
-    with open(read_user_css, 'r', encoding='UTF8') as f:
-        contents = f.readlines()
-    with open(write_user_css, 'w', encoding='UTF8') as f:
-        for c in contents:
-            f.write(c)
-
+def copy_user_css():
+    if os.path.isfile(write_css_folder + user_css):
+        os.rename(write_css_folder +  user_css, write_css_folder + user_css + ".old")    
+    shutil.copy(read_css_folder + user_css, write_css_folder + user_css)
+    
 def restore_user_css():
-    with open(write_user_css, 'w', encoding='UTF8') as f:
-        f.write("/* Put custom styles here */")
-
+    if os.path.isfile(write_css_folder + user_css):
+        os.remove(write_css_folder + user_css)
+    if os.path.isfile(write_css_folder + user_css + ".old"):
+        os.rename(write_css_folder + user_css + ".old", write_css_folder + user_css)
+    else:
+        with open(write_css_folder + user_css, 'w', encoding='UTF8') as f:
+            f.write("/* Put custom styles here */")
+            
 try:
     printout = "Uninstall"
     
